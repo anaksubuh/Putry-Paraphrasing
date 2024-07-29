8d903f193522822039c8f1d171b54f41a262624d
anaksubuh <johanesseptiawan2828@gmail.com>
2024-07-29 23:19:37 +0700

    first commit

---
+++ push.bat
@@ -0,0 +1,6 @@
+echo "# Putry-Paraphrasing" >> README.md
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/anaksubuh/Putry-Paraphrasing.git
+git push -u origin main
